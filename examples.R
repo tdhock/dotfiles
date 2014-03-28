e9c2d5824836c9a6368ac773a785a3a810a07c69
Toby Dylan Hocking <tdhock5@gmail.com>
2014-03-28 15:15:39 -0400

    works_with_R github ex

---
+++ examples.R
@@ -0,0 +1,20 @@
+## Usage examples for some of the functions defined in my .Rprofile.
+
+## Instead of library/require, use works_with_R to declare package
+## versions, for more reproducible analyses.
+works_with_R("3.0.2", RColorBrewer="1.0.5", ggplot2="0.9.3.1",
+             "tdhock/animint@f877163cd181f390de3ef9a38bb8bdd0396d08a4")
+
+## Convert a character vector to a named match matrix using a named
+## capture regular expression.
+subject <- c(
+  "tdhock/animint@057f34055ee876404caae7abc1c077a7bf126580",
+  "this subect should not match, resulting in a row of NA",
+  "rstudio/ggvis@8aa5ae207b3da0ff218fb5a3829bbdd59e54043c") 
+pattern <-
+  paste0("(?<GithubUsername>[^/]+)",
+         "/",
+         "(?<GithubRepo>[^@]+)",
+         "@",
+         "(?<GithubSHA1>[a-f0-9]{40})")
+str_match_perl(subject, pattern)
