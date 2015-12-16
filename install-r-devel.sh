c16d3150267196ae537977e7a050db2d1b4ff5b4
Toby Dylan Hocking <tdhock5@gmail.com>
2015-12-16 12:42:15 -0500

    ldconfig

--- install-r-devel.sh
+++ install-r-devel.sh
@@ -21,6 +21,11 @@ cd curl-7.46.0
 make
 make install
 
+## Very important: update global ldconfig cache, otherwise ldd will
+## not show the correct link to our files under $HOME/lib
+sudo ldconfig $HOME/lib
+## Otherwise, if we don't have sudo, try setting LD_LIBRARY_PATH.
+
 ## bz2, lzma development libraries now required.
 sudo aptitude install libbz2-dev liblzma-dev zlib1g-dev libcurl4-gnutls-dev
 
@@ -35,8 +40,16 @@ if [ -d R-devel ];then
 fi
 tar xf R-devel.tar.gz
 
-## Build and install R.
+## Build R.
 cd ~/R/R-devel
-CPPFLAGS=-I$HOME/include ./configure --prefix=$HOME --with-cairo
+CPPFLAGS="-I$HOME/include -L$HOME/lib" ./configure --prefix=$HOME --with-cairo
 make
-make install
+
+## I don't install R-devel, since I only use it for checking packages
+## before submission to CRAN. For the purposes of reproducible
+## research I would rather use a specific release version of R
+## (e.g. 3.2.2) and mention that explicitly in my code (rather than an
+## SVN commit number from R-devel, which can not be easily checked in
+## R code).
+
+## If libcurl works in R you should be able to do .Internal(curlDownload("https://www.bioconductor.org/packages/3.0/bioc/src/contrib/PACKAGES", "PACKAGES", FALSE, "w", TRUE))
