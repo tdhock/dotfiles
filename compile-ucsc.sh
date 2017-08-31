8fcc1d08ea2526d8abd61842340802bbbd13d8be
Toby Dylan Hocking <tdhock5@gmail.com>
2017-08-31 14:27:44 -0400

    revised ucsc tools compilation

--- compile-ucsc.sh
+++ compile-ucsc.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
+sudo aptitude install libmysqlclient-dev uuid-dev
 cd
-wget http://hgdownload.soe.ucsc.edu/admin/jksrc.zip
+if [ ! -e jksrc.zip ]; then curl -OL http://hgdownload.soe.ucsc.edu/admin/jksrc.zip;fi
+rm -rf kent
 unzip jksrc.zip
 export MACHTYPE=$(uname -m)
 mkdir -p ~/bin/$MACHTYPE
-cd kent/src/lib
-mkdir -p $MACHTYPE
-make
-cd ../jkOwnLib
-make
-sudo apt-get install libmysqlclient-dev
-cd ../utils
-for util in bigWigToBedGraph bedToBigBed bigWigInfo; do
+cd kent/src
+make libs
+cd utils
+for util in bigWigToBedGraph bedToBigBed bigWigInfo bedGraphToBigWig; do
     cd $util
     make
     cd ..
 done
 ## binaries installed to ~/bin/$MACHTYPE so add that to your PATH!
-echo ~/bin/$MACHTYPE
+echo INSTALLED TO ~/bin/$MACHTYPE
