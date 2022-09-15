d893527d06994dfa90838c57c7dc69807ec6feee
Toby Dylan Hocking <tdhock5@gmail.com>
2022-09-15 16:07:02 -0700

    rm empty entries

--- reformat-NEWS.R
+++ reformat-NEWS.R
@@ -1,5 +1,5 @@
 Sys.glob("*/NEWS")
-NEWS <- "animint2/NEWS"
+NEWS <- "~/teaching/regex-tutorial/NEWS/old/animint2.txt"
 change.dt <- nc::capture_all_str(
   NEWS,
   version="[0-9]+[.][0-9]+[.][0-9]+",
@@ -8,10 +8,21 @@ change.dt <- nc::capture_all_str(
 change.dt[, change.list := strsplit(changes, "\n\n")]
 change.dt[, new.str := sapply(change.list, function(change.vec){
   no.newline <- gsub("\n", " ", change.vec)
-  with.dash <- paste0("- ", no.newline)
+  no.empty <- grep("^\\s*$", no.newline, value=TRUE, invert=TRUE)
+  with.dash <- paste0("- ", no.empty)
   paste(with.dash, collapse="\n")
 })]
 change.dt[, new.block := sprintf(
   "Changes in version %s\n\n%s", version, new.str)]
 out.str <- paste(change.dt$new.block, collapse="\n\n")
 cat(out.str)
+old.dir <- dirname(NEWS)
+NEWS.dir <- dirname(old.dir)
+new.txt <- file.path(NEWS.dir, "new", basename(NEWS))
+cat(out.str, file=new.txt)
+bad.dt <- nc::capture_all_str(
+  new.txt,
+  dash_alone="\n-\\s*\n")
+if(nrow(bad.dt)){
+  stop("found ", nrow(bad.dt), " empty line(s)")
+}
